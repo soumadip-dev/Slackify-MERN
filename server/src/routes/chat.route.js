91eff6ebde7a3d592e3015177c2d3879fdb413a3
soumadip-dev <soumadipmajila@gmail.com>
2025-02-02 11:23:45 +0530

    feat: protect Stream token route with authentication middleware

--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getStreamToken } from '../controllers/chat.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 // This route is used to generate a Stream token for a user
-router.get('/token', getStreamToken);
+router.get('/token', protectRoute, getStreamToken);
 
 export default router;

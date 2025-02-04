4ff9012c352fb66be0673f4ea4c57e61e5c76b5a
soumadip-dev <soumadipmajila@gmail.com>
2025-02-04 10:35:18 +0530

    fix: resolve Axios error in api.js

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+// lib/api.ts
+import { axiosInstance } from './axios';
 
 // API call to get a Stream token for the current user.
 export async function getStreamToken() {
-  const response = await axios.get('/chat/token');
+  const response = await axiosInstance.get('/chat/token');
   return response.data;
 }

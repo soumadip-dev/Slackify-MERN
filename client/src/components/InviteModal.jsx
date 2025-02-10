8d0f2d5c0bd7254bb7b4026f45cb02469ae62714
soumadip-dev <soumadipmajila@gmail.com>
2025-02-10 13:50:40 +0530

    chore: add necessary imports in InviteModal.jsx

--- client/src/components/InviteModal.jsx
+++ client/src/components/InviteModal.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
+import { XIcon } from 'lucide-react';
 
 const InviteModal = () => {
-  return (
-    <div>InviteModal</div>
-  )
-}
+  return <div>InviteModal</div>;
+};
 
-export default InviteModal
+export default InviteModal;

41b80b181fb31b2ec2e482f14123e86b9a1db8be
soumadip-dev <soumadipmajila@gmail.com>
2025-02-21 14:22:18 +0530

    feat: add fetchUsers logic to UsersList with user filtering

--- client/src/components/UsersList.jsx
+++ client/src/components/UsersList.jsx
@@ -1,9 +1,25 @@
+import { useCallback } from 'react';
 import { useSearchParams } from 'react-router';
 import { useChatContext } from 'stream-chat-react';
 
-const UsersList = () => {
+const UsersList = ({ activeChannel }) => {
   const { client } = useChatContext();
   const [_, setSearchParams] = useSearchParams();
+
+  const fetchUsers = useCallback(async () => {
+    if (!client?.user) return;
+
+    const response = await client.queryUsers(
+      { id: { $ne: client.user.id } },
+      { name: 1 },
+      { limit: 20 }
+    );
+
+    const usersOnly = response.users.filter(user => !user.id.startsWith('recording-'));
+
+    return usersOnly;
+  }, [client]);
+
   return <div>UsersList</div>;
 };
 

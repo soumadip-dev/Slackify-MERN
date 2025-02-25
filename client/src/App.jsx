d584a89adbd0a697999d08c27a5ec1f9a7146037
soumadip-dev <soumadipmajila@gmail.com>
2025-02-25 13:05:33 +0530

    refactor: remove SignedIn component from App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';

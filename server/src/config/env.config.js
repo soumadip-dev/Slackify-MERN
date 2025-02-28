64f942ccd14a863514458c5a54ef97921185e662
soumadip-dev <soumadipmajila@gmail.com>
2025-02-28 09:15:22 +0530

    chore: add STREAM_API_KEY and STREAM_API_SECRET to config

--- server/src/config/env.config.js
+++ server/src/config/env.config.js
@@ -8,4 +8,6 @@ export const ENV = {
   NODE_ENV: process.env.NODE_ENV,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
 };

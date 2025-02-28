7215b4c2776b71f4338d05a4b5c64c3f44b964de
soumadip-dev <soumadipmajila@gmail.com>
2025-02-28 11:23:45 +0530

    chore: add Clerk publisher and secret keys to env.js

--- server/src/config/env.config.js
+++ server/src/config/env.config.js
@@ -6,4 +6,6 @@ export const ENV = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_PUBLISHABLE_KEY,
 };

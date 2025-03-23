fcd41f727bd44ab2b75a8b8b0b5b86f27bacf76d
soumadip-dev <soumadipmajila@gmail.com>
2025-03-23 10:35:18 +0530

    refactor: split server.js into app and server files

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,7 @@
-import express from 'express';
-import cors from 'cors';
+import { ENV } from '../config/env.config.js';
+import app from './App.js';
 
-const app = express();
-
-const PORT = 8080;
-
-//* Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-//* Root Route
-app.get('/', (req, res) => {
-  res.send('Hello from SLACKLY backend!');
-});
+const PORT = ENV.PORT || 8080;
 
 app.listen(PORT, () => {
   console.info(`✔️ Server is up and running on port: ${PORT}`);

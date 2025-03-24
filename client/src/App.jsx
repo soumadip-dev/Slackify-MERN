f246f1d9eb4b1f5bb4a17a89dce9e1f2725960f5
soumadip-dev <soumadipmajila@gmail.com>
2025-03-24 10:15:47 +0530

    feat: add Clerk authentication components to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,14 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton mode="modal"/>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 };
 export default App;

4139983239c78c1aad0a9945d2979f86cbf2c7f4
soumadip-dev <soumadipmajila@gmail.com>
2025-03-25 13:30:45 +0530

    feat: add Clerk middleware to Express server

--- server/src/App.js
+++ server/src/App.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
 
 //* Root Route
 app.get('/', (req, res) => {

4eecd80d4bac1762d93a537f380d03fc21bf1cec
soumadip-dev <soumadipmajila@gmail.com>
2025-03-26 15:35:47 +0530

    chore: add VITE_STREAM_API_KEY to .env and import in useStreamChat.js

--- client/src/hooks/useStreamChat.js
+++ client/src/hooks/useStreamChat.js
@@ -3,3 +3,5 @@ import { StreamChat } from 'stream-chat';
 import { useUser } from '@clerk/clerk-react';
 import { useQuery } from '@tanstack/react-query';
 import { getStreamToken } from '../lib/api';
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;

53c516df109a53dbc8417d49c793699597afcf30
soumadip-dev <soumadipmajila@gmail.com>
2025-03-26 15:35:47 +0530

    docs: add comments to getStreamToken function

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// API call to get a Stream token for the current user.
 export async function getStreamToken() {
   const response = await axios.get('/chat/token');
   return response.data;

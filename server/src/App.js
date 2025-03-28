19e07839de3fb19ef03b34bda2542edef897df49
soumadip-dev <soumadipmajila@gmail.com>
2025-03-28 12:53:27 +0530

    feat: integrate Inngest functions with Express using /api/inngest route

--- server/src/App.js
+++ server/src/App.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express';
+import { inngest, functions } from './config/inngest.config.js';
 
 const app = express();
 
@@ -8,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware());
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 //* Root Route
 app.get('/', (req, res) => {

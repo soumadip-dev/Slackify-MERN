14c63647f248ab6c70916ab5fe5de9e67f689060
soumadip-dev <soumadipmajila@gmail.com>
2025-03-28 13:45:39 +0530

    refactor: merge app.js into server.js to simplify entry point

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express';
+import { inngest, functions } from './config/inngest.config.js';
 import { ENV } from './config/env.config.js';
-import app from './App.js';
 import { connectDb } from './config/db.config.js';
+
+const app = express();
+
+//* Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
+
+//* Root Route
+app.get('/', (req, res) => {
+  res.send('Hello from SLACKIFY backend!');
+});
+
+//* Routes
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 const PORT = ENV.PORT || 8080;
 
@@ -20,3 +40,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+export default app;

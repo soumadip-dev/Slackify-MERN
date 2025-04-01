aa6c543995267d85f892e9b1aabc3c85fd100003
soumadip-dev <soumadipmajila@gmail.com>
2025-04-01 15:35:47 +0530

    feat: implement initial HomePage with Clerk user button and StreamChat hook

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,16 @@
 import { UserButton } from '@clerk/clerk-react';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router';
+import { useStreamChat } from '../hooks/useStreamChat';
 
 const HomePage = () => {
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false); // Check if model is open or not
+  const [activeChannel, setActiveChannel] = useState(null);
+  
+  const [searchParams, setSearchParams] = useSearchParams(); // Get search params from URL
+
+  const { chatClient, error, isLoading } = useStreamChat(); // Get chat client from hook
+
   return (
     <div>
       <p>Home Page</p>

2a0c741effb12ff5e2664286bc924a2feb7278d3
soumadip-dev <soumadipmajila@gmail.com>
2025-04-01 15:35:47 +0530

    feat: set active channel in HomePage using URL params and StreamChat client

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,29 @@
 import { UserButton } from '@clerk/clerk-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
 import { useStreamChat } from '../hooks/useStreamChat';
 
 const HomePage = () => {
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false); // Check if model is open or not
   const [activeChannel, setActiveChannel] = useState(null);
-  
+
   const [searchParams, setSearchParams] = useSearchParams(); // Get search params from URL
 
   const { chatClient, error, isLoading } = useStreamChat(); // Get chat client from hook
+
+  // Set active channel from URL params
+  useEffect(() => {
+    if (chatClient) {
+      const channelId = searchParams.get('channel');
+      if (channelId) {
+        const channel = chatClient.channel('messaging', channelId);
+        setActiveChannel(channel);
+      }
+    }
+  }, [chatClient, searchParams]);
+
+  // todo: handle this with a better component
+  if (error) return <p>Something went wrong...</p>;
 
   return (
     <div>

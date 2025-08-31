518b5c724ff236b5a819dee2bc00669dae25371c
soumadip-dev <soumadipmajila@gmail.com>
2025-08-31 09:41:18 +0530

    feat: add React Hot Toast Toaster in main entry file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')).render(
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <BrowserRouter>
           <App />
+          <Toaster />
         </BrowserRouter>
       </ClerkProvider>
     </QueryClientProvider>

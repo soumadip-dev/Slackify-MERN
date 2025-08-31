26e4f7598051cb4f5e415b72e801e61a59956c72
soumadip-dev <soumadipmajila@gmail.com>
2025-08-31 15:47:21 +0530

    refactor: update route handling to use conditional rendering with useAuth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,22 @@
-import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, useAuth } from '@clerk/clerk-react';
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 import CallPage from './pages/CallPage';
 const App = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return null;
   return (
-    <>
-      <SignedIn>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<Navigate to="/" replace />} /> // replaces history instead
-          of pushing new entry
-          <Route path="/call" element={<CallPage />} />
-        </Routes>
-      </SignedIn>
-      <SignedOut>
-        <Routes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="*" element={<Navigate to="/auth" replace />} />
-        </Routes>
-      </SignedOut>
-    </>
+    <Routes>
+      <Route path="/" element={isSignedIn ? <HomePage /> : <Navigate to={'/auth'} replace />} />
+      <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={'/'} replace />} />
+      <Route path="/call" element={isSignedIn ? <CallPage /> : <Navigate to={'/auth'} replace />} />
+      <Route
+        path="*"
+        element={isSignedIn ? <Navigate to={'/'} replace /> : <Navigate to={'/auth'} replace />}
+      />
+    </Routes>
   );
 };
 export default App;

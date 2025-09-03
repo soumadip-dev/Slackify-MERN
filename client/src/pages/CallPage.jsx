c5790771f875b0114cee7e16c48a2a24ac1d4d12
soumadip-dev <soumadipmajila@gmail.com>
2025-09-03 10:30:00 +0530

    chore: add necessary imports to CallPage component

--- client/src/pages/CallPage.jsx
+++ client/src/pages/CallPage.jsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import { useUser } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+
+import { getStreamToken } from '../lib/api';
 
 const CallPage = () => {
   return <div>CallPage</div>;

01e59041f05705d2a05bc7a3c489f88f6906b523
soumadip-dev <soumadipmajila@gmail.com>
2025-09-03 10:30:00 +0530

    chore: add Stream Video SDK imports and styles to CallPage

--- client/src/pages/CallPage.jsx
+++ client/src/pages/CallPage.jsx
@@ -6,6 +6,20 @@ import toast from 'react-hot-toast';
 
 import { getStreamToken } from '../lib/api';
 
+import {
+  StreamVideo,
+  StreamVideoClient,
+  StreamCall,
+  CallControls,
+  SpeakerLayout,
+  StreamTheme,
+  CallingState,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+
+// import styles from stream video
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+
 const CallPage = () => {
   return <div>CallPage</div>;
 };

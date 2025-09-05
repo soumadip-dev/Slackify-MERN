90ec6cf11c8c277cd09c206665e6569b5c5eb418
soumadip-dev <soumadipmajila@gmail.com>
2025-09-05 13:32:50 +0530

    feat: integrate StreamVideo and CallContent into CallPage with connection handling

---
+++ client/src/components/CallContent.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router';
+
+import {
+  CallControls,
+  SpeakerLayout,
+  StreamTheme,
+  CallingState,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+
+const CallContent = () => {
+  const { useCallCallingState } = useCallStateHooks();
+
+  const callingState = useCallCallingState();
+  const navigate = useNavigate();
+
+  if (callingState === CallingState.LEFT) return navigate('/');
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout />
+      <CallControls />
+    </StreamTheme>
+  );
+};
+
+export default CallContent;
+
+
